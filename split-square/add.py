35b38d70537b3bd828c8729eb26af6e956bc79bd
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-10-10 14:58:56 -0500

    ready to submit

---
+++ split-square/add.py
@@ -0,0 +1,67 @@
+"""Produce new square adding two inputs squares.
+
+Two simple squares can be added::
+
+    >>> s1 = 0
+    >>> s2 = 1
+
+    >>> add(s1, s2)
+    1
+
+A simple square and a split square can be added::
+
+    >>> s1 = 0
+    >>> s2 = [1, 0, 1, 0]
+
+    >>> add(s1, s2)
+    [1, 0, 1, 0]
+
+Two split squares can be added::
+
+    >>> s1 = [0, 0, 0, 1]
+    >>> s2 = [0, 1, 0, 1]
+
+    >>> add(s1, s2)
+    [0, 1, 0, 1]
+
+Nested squares can be added::
+
+    >>> s1 = [0, [1, 1, 1, [0, 0, 0, 0]], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+Unevenly-nested squares can be added::
+
+    >>> s1 = [0, [1, 1, 1, 0           ], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> s1 = [0, [1, 1, 1, 1                      ], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, [0, 0, 0, 0], 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [1, [1, 1, 1, 1], 1, 1]], [1, 0, 1, 0], 1]
+"""
+
+
+def add(s1, s2):
+    """Produce new split square adding two input squares."""
+    if type(s1)==int and type(s2)== int:
+        return s1 | s2
+
+    if type(s1) == int:
+        s1 = [s1, s1, s1, s1]
+
+    if type(s2) == int:
+        s2 = [s2, s2, s2, s2]
+        
+    return [add(cell1, cell2) for cell1, cell2 in zip(s1, s2)]
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print ("\n*** ALL TESTS PASS; YOU'RE A RECURSION WIZARD!\n")
