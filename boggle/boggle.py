3be251665fe07eb64852c0e84c26678abee0c42b
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-10-11 08:53:31 -0500

    finished boggle

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -107,11 +107,86 @@ def make_board(board_string):
 
     return board
 
+def find_from(board, word, y, x, seen):
+    """Can we find a word on board, starting at x, y?"""
+
+    # This is called recursively to find smaller and smaller words
+    # until all tries are exhausted or until success.
+
+    # Base case: this isn't the letter we're looking for.
+
+    if board[y][x] != word[0]:
+        return False
+
+    # Base case: we've used this letter before in this current path
+
+    if (y, x) in seen:
+        return False
+
+    # Base case: we are down to the last letter --- so we win!
+
+    if len(word) == 1:
+        return True
+
+    # Otherwise, this letter is good, so note that we've seen it,
+    # and try of all of its neighbors for the first letter of the
+    # rest of the word
+
+
+    # This next line is a bit tricky: we want to note that we've seen the
+    # letter at this location. However, we only want the child calls of this
+    # to get that, and if we used `seen.add(...)` to add it to our set,
+    # *all* calls would get that, since the set is passed around. That would
+    # mean that once we try a letter in one call, it could never be tried again,
+    # even in a totally different path. Therefore, we want to create a *new*
+    # seen set that is equal to this set plus the new letter. Being a new
+    # object, rather than a mutated shared object, calls that don't descend
+    # from us won't have this `y,x` point in their seen.
+    #
+    # To do this, we use the | (set-union) operator, read this line as
+    # "rebind seen to the union of the current seen and the set of point(y,x))."
+    #
+    # (this could be written with an augmented operator as "seen |= {(y, x)}",
+    # in the same way "x = x + 2" can be written as "x += 2", but that would seem
+    # harder to understand).
+
+    seen = seen | {(y, x)}
+
+    if y > 0:
+        if find_from(board, word[1:], y - 1, x, seen):
+            return True
+
+    if y < 4:
+        if find_from(board, word[1:], y + 1, x, seen):
+            return True
+
+    if x > 0:
+        if find_from(board, word[1:], y, x - 1, seen):
+            return True
+
+    if x < 4:
+        if find_from(board, word[1:], y, x + 1, seen):
+            return True
+
+    # Couldn't find the next letter, so this path is dead
+
+    return False
 
 
 def find(board, word):
     """Can word be found in board?"""
 
+    # Find starting letter --- try every spot on board and,
+    # win fast, should we find the word at that place.
+
+    for y in range(0, 5):
+        for x in range(0, 5):
+            if find_from(board, word, y, x, seen=set()):
+                return True
+
+    # We've tried every path from every starting square w/o luck.
+
+    return False
 
 if __name__ == '__main__':
     import doctest
